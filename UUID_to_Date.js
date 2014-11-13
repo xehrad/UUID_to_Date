eba7d3ca024529e652feea1a8b1f5fa2b8f2f2b3
Mehrad <col.on.d.a.sh.d+github@gmail.com>
2014-11-14 02:08:42 +0330

    rename function

--- UUID_to_Date.js
+++ UUID_to_Date.js
@@ -1,7 +1,7 @@
 (function() {
 	var GREGORIAN_OFFSET = 122192928000000000;
 	this.UUID_to_Date = {
-		uuid__to__time_int: function (uuid_str) {
+		get_time_int: function (uuid_str) {
 			// uuid_str like	=>		'11111111-2222-#333-4444-555555555555'
 			var uuid_arr = uuid_str.split('-'),
 			
@@ -15,10 +15,10 @@
 			return parseInt( time_str, 16 );
 		},
 		
-		uuid__to__date_obj: function (uuid_str) {
+		get_date_obj: function (uuid_str) {
 			// uuid_str like	=>		'11111111-2222-#333-4444-555555555555'
 			// set_TZO: Boolean	=>		if set True, add client Timezone, you can use 
-			var int_time = this.uuid__to__time_int( uuid_str ) - GREGORIAN_OFFSET,
+			var int_time = this.get_time_int( uuid_str ) - GREGORIAN_OFFSET,
 				int_millisec = Math.floor( int_time/10000 );
 			return new Date( int_millisec );
 		}
