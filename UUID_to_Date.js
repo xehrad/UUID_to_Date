be37b3bb7bbbe274d80276b2de5236c17393798a
Mehrad <col.on.d.a.sh.d+github@gmail.com>
2014-11-13 23:49:38 +0330

    Update UUID_to_Date.js

--- UUID_to_Date.js
+++ UUID_to_Date.js
@@ -1,14 +1,6 @@
 (function() {
 	var GREGORIAN_OFFSET = 122192928000000000;
 	this.UUID_to_Date = {
-		secs__to__date_obj: function (secs, set_TZO) {
-			var date_obj = new Date( 1970, 0, 1 ),
-				TZs = set_TZO  ?  date_obj.getTimezoneOffset() * 60  :  0;
-			
-			date_obj.setSeconds( secs - TZs );
-			return date_obj;
-		},
-		
 		uuid__to__time_int: function (uuid_str) {
 			// uuid_str like	=>		'11111111-2222-#333-4444-555555555555'
 			var uuid_arr = uuid_str.split('-'),
@@ -23,18 +15,12 @@
 			return parseInt( time_str, 16 );
 		},
 		
-		uuid__to__date_obj_manual_timezone: function (uuid_str, set_TZO) {
+		uuid__to__date_obj: function (uuid_str) {
 			// uuid_str like	=>		'11111111-2222-#333-4444-555555555555'
 			// set_TZO: Boolean	=>		if set True, add client Timezone, you can use 
 			var int_time = this.uuid__to__time_int( uuid_str ) - GREGORIAN_OFFSET,
-				int_sec_time = Math.floor( int_time/10000000 );
-			
-			return this.secs__to__date_obj( int_sec_time, set_TZO );
-		},
-			
-		uuid__to__date_obj: function (uuid_str) {
-			// uuid_str like	=>		'11111111-2222-#333-4444-555555555555'
-			return this.uuid__to__date_obj_manual_timezone( uuid_str, true );
+				int_millisec = Math.floor( int_time/10000 );
+			return new Date( int_millisec );
 		}
 	};
 }).call(this);
